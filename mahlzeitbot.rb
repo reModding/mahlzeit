93044d9b1d35209c5137cd9d1d66750115f88d00
Johannes Eiglsperger <git@remodding.de>
2014-07-17 21:45:38 +0200

    Konfigurationsdatei, erste Features

--- mahlzeitbot.rb
+++ mahlzeitbot.rb
@@ -1,13 +1,15 @@
 #!/usr/bin/env ruby
 
 require 'socket'
+require 'yaml'
 
 class MyBot
-  def initialize(server, port, channel, nick, login, username)
+  def initialize(server, port, channel, nick)
     @channel = channel
+    @nick = nick
     @socket = TCPSocket.open(server, port)
-    say "NICK #{nick}"
-    say "USER #{login} 8 * :#{username}"
+    say "NICK #{@nick}"
+    say "USER #{@nick} 8 * :#{@nick}"
     say "JOIN #{@channel}"
     say_to_chan "#{1.chr}ACTION is here to test#{1.chr}"
   end
@@ -46,17 +48,62 @@ class MyBot
         next
       end
 
-      if msg.match(/PRIVMSG #{@channel} :(.*)$/)
-        content = $~[1]
-	if content.match(/\+help"/)
-	  say_to_chan('+orte     - gibt eine Liste aller Orte aus, die ich kenne.')
-	  say_to_chan('+1 ort    - stimmt fuer den Ort.')
-	  say_to_chan('+stand    - gibt den aktuellen Punketstand aus.')
-	  say_to_chan('+wasgibts - schickt den aktuellen Intra-Link.')
-	  say_to_chan('+werfehlt - zeigt alle an, die noch nicht gevotet haben.')
+      if msg.match(/^:(.*)!(.*) PRIVMSG #{@channel} :(.*)$/)
+	nick = $~[1]
+	login = $~[2]
+        content = $~[3]
+
+	if nick == @nick
+	  next
 	end
+
+	if content.match(/\+help/)
+	  help
+	end
+
+	if content.match(/\+orte/)
+	end
+
+	if content.match(/\+stand/)
+	end
+
+	if content.match(/\+wasgibts/)
+	  wasgibts(nick)
+	end
+
+	if content.match(/\+werfehlt/)
+	end
+
+	if content.match(/\+add (.*)/)
+	  loc = $~[1]
+	end
+
+	if content.match(/\+del (.*)/)
+	  loc = $~[1]
+	end
+
+	if content.match(/\+([0-9]*) (.*)/)
+	  votes = $~[1]
+	  voted_loc = $~[2]
+	end
+
+        next
       end
     end
+  end
+
+  def help
+    say_to_chan "+orte     - gibt eine Liste aller Orte aus, die ich kenne."
+    say_to_chan "+1 ORT    - stimmt fuer den Ort."
+    say_to_chan "+stand    - gibt den aktuellen Punktestand aus."
+    say_to_chan "+wasgibts - schickt den aktuellen Intra-Link."
+    say_to_chan "+werfehlt - zeigt alle an, die noch nicht gevotet haben."
+    say_to_chan "+add ORT  - fuegt einen Ort hinzu."
+    say_to_chan "+del ORT  - entfernt einen Ort."
+  end
+
+  def wasgibts nick
+    say_to_chan("#{nick}, schau bitte hier: http://intra.space.net/x/lQFl")
   end
 
   def quit
@@ -65,7 +112,9 @@ class MyBot
   end
 end
 
-bot = MyBot.new("irc.space.net", 6667, '#rudeltest', "MahlzeitT", "MahlzeitT", "MahlzeitBot")
+
+config = YAML.load_file("mahlzeitbot.yml")
+bot = MyBot.new(config["irc"]["server"], config["irc"]["port"], config["irc"]["channel"], config["irc"]["nick"])
 
 trap("INT"){ bot.quit }
 
